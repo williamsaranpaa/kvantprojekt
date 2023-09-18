24f23a3c8e5f38272a8718ab15eb2c8985300e16
williamsaranpaa <william.saranpaa@gmail.com>
2023-09-18 17:03:45 +0200

    initialised the matrices as in the task

---
+++ time-dependent-shrodinger/main.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+L=6
+V=-1
+ep1=2
+#t from 0 to 20
+
+
+# initialising the H0 operator, should have V as a value on both ofset diagonals as in (12)
+H0 = np.zeros((L, L))
+
+for i in range(L):
+    if i+1<=L-1:
+        H0[i, i+1]=V
+    if i-1>=0:
+        H0[i, i-1]=V
+
+print(H0)
+print('\n')
+
+# initialising the hamiltonian for times larger than 0
+# H1 is just the value eps1 on the the site (0,0)
+# H is H0 + H1
+H1 = np.zeros((L,L))
+
+H1[0,0]=ep1
+
+H=H0 + H1
+
+print(H)
